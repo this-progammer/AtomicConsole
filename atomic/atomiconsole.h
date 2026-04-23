c5765294f23b7334dad98faecde9b2812477de22
*(this)-programmer == i++ <140750964+this-progammer@users.noreply.github.com>
2026-04-23 08:25:48 -0500

    Update atomiconsole.h

--- atomic/atomiconsole.h
+++ atomic/atomiconsole.h
@@ -6,11 +6,36 @@
 #define CONSOLE_MAX 9999
 #define CONSOLECHAR const char
 
+/*
+CONSOLECHAR const:
+(_<CONSOLECHAR>);
+*/
+
 class Console {
 public:
-  Console(CONSOLECHAR* _arg);
+  Console(const char* arg);
   ~Console() override;
 
+/*get console*/
+inline Console& getConsole() {
+  return *this;
+}
+
+protected:
+const char* ARG;
+CONSOLECHAR const* _ARG;
+bool CLEARED;
+}
+
+inline void useConsoleCharRule() {
+  char* ARG_COUNT[100];
+  for( auto i; i <= 0; ++i ) {
+       while(ARG_COUNT[i][0] && ARG_COUNT[i][99] == "("[i][0]+")"[i][99]) {
+           if( true ) {
+              ARG_COUNT[i][1] > ARG_COUNT[i][0] and < ARG_COUNT[i][99];
+           }
+       } 
+  }
 }
 
 #endif
