6ccce68ff4503548dac80770bdaf7d6ae6c8cbee
*(this)-programmer == i++ <140750964+this-progammer@users.noreply.github.com>
2026-04-24 10:10:41 -0500

    Update variable.cpp

--- atomic/variable.cpp
+++ atomic/variable.cpp
@@ -2,23 +2,30 @@
 
 #include "atomiconsole.h"
 
-const char* g_VarChar = "type";
-
-//* init token
-ConsoleToken::ConsoleToken() : ( this->d_console ) {
-  *this->char_token = g_VarChar;
-}
+// *globals
+const char* g_VarChar = "type"; // keyword
+const char* g_NewTypeName; // new variable name
 
 //*.token
 const char* ConsoleToken::createVariableToken() {
   Console& box = this( g_VarChar )->d_console;
   box.ARG( box.char_token );
-
   if ( box( this->d_console.ARG ) ) {
-    if ( g_VarChar+"\t"+"%5.2c" ) { 
-         return new char;
+    if ( g_VarChar+" "+"%5.2c"( g_NewTypeName ) ) {
+       return new char;
+       if( [g_NewTypeName] + " " + "=" + "%i" ) {
+          reinterpret_cast<int>(g_NewTypeName);
+          return;
+       }
+       if( [g_NewTypeName] + " " + "=" + "%b" ) {
+          reinterpret_cast<bool>( g_NewTypeName );
+          return;
+       }
+       if( [g_NewTypeName] + " " + "=" + "%f" ) {
+          reinterpret_cast<float>( g_NewTypeName );
+          return;
+       }
     }
   }
-  
   return g_VarChar;
 }
