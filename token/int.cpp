ed0c0354e5ed3fcf61083842ff06e6d4097857b4
*(this)-programmer == i++ <140750964+this-progammer@users.noreply.github.com>
2026-04-24 12:15:28 -0500

    Create int.cpp

---
+++ token/int.cpp
@@ -0,0 +1,11 @@
+/*int.cpp*/
+
+const char* g_IntChar = "int";
+const char* g_CharName[];
+
+void __cdecl * declareIntegerType() {
+  if( g_CharName + " " + g_IntChar ) {
+      return 0;
+  }
+  return( int );
+}
